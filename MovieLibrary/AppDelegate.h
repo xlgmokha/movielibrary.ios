b5c007634e6374bccb3ede85d05e892f45cfeede
mo khan <mo@mokhan.ca>
2013-05-14 19:08:11 -0600

    initial commit

---
+++ MovieLibrary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MovieLibrary
+//
+//  Created by mo khan on 2013-05-14.
+//  Copyright (c) 2013 mo khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MovieLibraryTests/MovieLibraryTests.h
@@ -0,0 +1,13 @@
+//
+//  MovieLibraryTests.h
+//  MovieLibraryTests
+//
+//  Created by mo khan on 2013-05-14.
+//  Copyright (c) 2013 mo khan. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MovieLibraryTests : SenTestCase
+
+@end
