d5892e4489858e899aa854cb273f70a2dfc0f244
mo khan <mo@mokhan.ca>
2013-05-15 21:55:31 -0600

    first failing test for the movie library

---
+++ MovieLibraryTests/MovieLibrarySpec.m
@@ -0,0 +1,38 @@
+#import "Kiwi.h"
+
+
+@interface Book : NSObject
+@end
+
+@implementation Book
+@end
+
+@interface MovieLibrary : NSObject
+-(void)add:(Book*)book;
+-(int)total_books;
+@end
+
+@implementation MovieLibrary
+-(void)add:(Book*)book
+{
+}
+-(int)total_books
+{
+  return 0;
+}
+@end
+
+
+SPEC_BEGIN(MovieLibrarySpec)
+
+describe(@"MovieLibrary", ^{
+    it(@"should be able to add a book", ^{
+      MovieLibrary * library = [MovieLibrary new];
+      Book *book = [Book new];
+      [library add:book];
+      int result = [library total_books];
+      [[theValue(result) should] equal:theValue(1)];
+    });
+});
+
+SPEC_END
