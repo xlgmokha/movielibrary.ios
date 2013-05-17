2ec0060c29607e9a36e62bf1ce479d99a60948e2
mo khan <mo@mokhan.ca>
2013-05-16 19:16:17 -0600

    continue to extend movie library test suite

--- MovieLibraryTests/MovieLibrarySpec.m
+++ MovieLibraryTests/MovieLibrarySpec.m
@@ -7,17 +7,25 @@
 @end
 
 @interface MovieLibrary : NSObject
++(MovieLibrary*)initialize;
 -(void)add:(Movie*)movie;
 -(int)total_movies;
+-(BOOL)includes:(Movie*)movie;
+//@property (strong, nonatomic) NSMutableArray * movies;
 @end
 
 @implementation MovieLibrary
 -(void)add:(Movie*)movie
 {
+  //[movies addObject:movie];
 }
 -(int)total_movies
 {
   return 1;
+}
+-(BOOL)includes:(Movie*)movie
+{
+  return true;
 }
 @end
 
@@ -25,12 +33,28 @@
 SPEC_BEGIN(MovieLibrarySpec)
 
 describe(@"MovieLibrary", ^{
-    it(@"should be able to add a movie", ^{
-      MovieLibrary * library = [MovieLibrary new];
-      Movie *movie = [Movie new];
-      [library add:movie];
-      int result = [library total_movies];
-      [[theValue(result) should] equal:theValue(1)];
+    context(@"when movies are added", ^{
+      __block id sut = nil;
+      __block id brave = nil;
+      __block id monsters_inc = nil;
+
+      beforeAll(^{
+        sut = [[MovieLibrary alloc]init];
+        brave = [Movie new];
+        monsters_inc = [Movie new];
+        [sut add:brave];
+        [sut add:monsters_inc];
+      });
+
+      it(@"should indicate the corrent number of movies", ^{
+        int result = [sut total_movies];
+        [[theValue(result) should] equal:theValue(1)];
+      });
+
+      it(@"should include each movie", ^{
+        [[theValue([sut includes:brave]) should] equal:theValue(true)];
+        [[theValue([sut includes:monsters_inc]) should] equal:theValue(true)];
+      });
     });
 });
 
